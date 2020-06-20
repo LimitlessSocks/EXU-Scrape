60aa8c6bc700eb746a9f465c5a5f45ec49d8880a
LimitlessSocks <socktheringleader@gmail.com>
2020-06-20 04:20:08 -0400

    update banlist resources

--- banlist.js
+++ banlist.js
@@ -1,1 +1,10 @@
-// todo
+let onLoad = async function () {
+    let response = await fetch("https://raw.githubusercontent.com/LimitlessSocks/EXU-Scrape/master/banlist.json");
+    let db = await response.json();
+    CardViewer.Database.setInitial(db);
+    
+    $("body").append(
+        CardViewer.composeResult(CardViewer.Database.cards[9138])
+    );
+};
+window.addEventListener("load", onLoad);
