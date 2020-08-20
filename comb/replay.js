288db07389020f9620c33aa3906f419e2c72f23d
LimitlessSocks <socktheringleader@gmail.com>
2020-08-20 16:47:09 -0400

    add replay scraper

---
+++ comb/replay.js
@@ -0,0 +1,99 @@
+(function () {
+    if(replay_arr.length === 0) return null;
+    if(!$("#pause_replay_btn").attr("disabled")) {
+        $("#pause_replay_btn").click();
+    }
+    
+    // local copy
+    let replay_actions = replay_arr.slice();
+    
+    const match = (compareProps, targetProps) => {
+        for(let [key, val] of Object.entries(compareProps)) {
+            if(targetProps[key] !== val) return false;
+        }
+        return true;
+    }
+    const findBy = (props, arr = replay_arr) =>
+        arr.find(el => match(props, el));
+    
+    let players = findBy({ play: "Pick first" }).order;
+    let roundsWon = {};
+    for(let player of players) {
+        roundsWon[player] = 0;
+    }
+    
+    // parse into games
+    const isDeterminator = (obj) =>
+        obj.play === "Admit defeat" ||
+        obj.play === "Quit duel";
+    
+    let canParse = true;
+    
+    const collectGame = () => {
+        if(!canParse) {
+            console.log("Can no longer parse games, as the match is over.");
+            return null;
+        }
+        let actions = [];
+        while(replay_actions.length && !isDeterminator(replay_actions[0])) {
+            actions.push(replay_actions.shift());
+        }
+        let det = replay_actions.shift();
+        console.log("Determinator:", det);
+        let loser = det.username;
+        console.log("Round loser:", loser);
+        let winner = players[loser === players[0] ? 1 : 0]
+        console.log("Round winner:", winner);
+        roundsWon[winner]++;
+        
+        // info for next collect
+        if(det.over || replay_actions.length === 0) {
+            canParse = false;
+        }
+        
+        return {
+            winner: winner,
+            loser: loser,
+            actions: actions,
+        };
+    };
+    let game1 = collectGame();
+    let game2 = collectGame();
+    let game3 = collectGame();
+    
+    console.log("Rounds won:", roundsWon);
+    let games = [game1, game2, game3].filter(e => e);
+    
+    return {
+        players: players,
+        games: games,
+        roundsWon: roundsWon,
+    };
+    
+    /*
+    // test if easy winner
+    let simpleDefeat = findBy({
+        play: "Admit defeat",
+        over: true
+    });
+    
+    // the player who admits defeat and causes a game to be over loses
+    if(simpleDefeat) {
+        loser = simpleDefeat.username;
+    }
+    else {
+        // if there is no admitted defeat, the player who quits first loses
+        loser = findBy({ play: "Quit duel" }).username;
+    }
+    winner = players[loser === players[0] ? 1 : 0];
+    
+    // a winner has now been determined
+    if(winner) {
+        console.log("Winner:", winner);
+        console.log("Loser:", loser);
+    }
+    
+    // view cards used by the winner and loser
+    return winner;
+    */
+})();
--- scrape-replay.rb
+++ scrape-replay.rb
@@ -1,6 +1,12 @@
-# jscode= 
+$comb_replay = File.read("comb/replay.js")
+
 # plays = ["To GY", "SS ATK", "SS DEF", "Activate ST", "Declare",]
 # replay_arr.filter(e => plays.indexOf(e.play))
+require 'json'
+require 'capybara'
+puts "Loading capybara..."
+$session = Capybara::Session.new(:selenium)
+puts "Loaded!"
 
 weekly11 = [
     "343487-20661105",
@@ -21,3 +27,137 @@ weekly11 = [
     "14422-20664735",
     "332143-20666201",
 ]
+
+ycs_dino_devestation = [
+    "534677-20358748",
+    "453279-20358512",
+    "453279-20368153",
+    "453279-20342563",
+    "509709-20336290",
+    "282034-20334065",
+    "534677-20328742",
+    "106930-20327304",
+    "122221-20321965",
+    "534677-20302578",
+    "509709-20298155",
+    "481907-20297741",
+    "106930-20294493",
+    "332143-20285453",
+    "479696-20282953",
+    "122221-20245875",
+    "481907-20242298",
+    "453279-20218752",
+    "509709-20216464",
+    "474911-20209140",
+    "332143-20205828",
+    "481907-20276153",
+    "474911-20275626",
+    "123-20274886",
+    "349034-20273099",
+    "453279-20263864",
+    "106930-20263685",
+    "282034-20258524",
+    "192142-20257417",
+    "332143-20247226",
+    "123-20199561",
+    "14422-20199440",
+    "192142-20193869",
+    "106930-20186553",
+    "343043-20183028",
+    "509709-20181680",
+    "205781-20175212",
+    "453279-20168782",
+    "356-20167424",
+    "479696-20167096",
+    "441001-20166224I",
+]
+
+focus = ycs_dino_devestation
+name = "ycs_dino_devestation"
+# focus = weekly11
+# name = "weekly11"
+
+$database = {}
+
+$cards_total_count = Hash.new 0
+$cards_win_count = Hash.new 0
+$valid_actions = [
+    "To GY", "Activate ST", "Declare", "To ST",
+    "Normal Summon",
+    "SS ATK", "SS DEF",
+    "OL ATK", "OL DEF",
+    "Activate Field Spell",
+]
+
+focus.each { |link|
+    $session.visit("https://www.duelingbook.com/replay?id=#{link}")
+    data = nil
+    while data.nil?
+        data = $session.evaluate_script $comb_replay
+    end
+
+    data["games"].each { |game|
+        winner = game["winner"]
+        loser = game["loser"]
+        actions = game["actions"]
+        cards_appear = {}
+        
+        actions.select! { |action|
+            card = action["card"]
+            # log = action["log"]
+            play = action["play"]
+            card and play and $valid_actions.include?(play)
+            # card and log and log["public_log"].include?(card["name"])
+        }
+        actions.each { |action|
+            card = action["card"]
+            id = card["id"]
+            #override id
+            orig_label = $database.find { |id, name| name == card["name"] }
+            if orig_label
+                id = orig_label[0]
+            end
+            
+            next if id.zero? #Skip tokens
+            
+            username = action["username"]
+            id_unique = "#{id}.#{username}"
+            
+            $database[id] ||= card["name"]
+            
+            next if cards_appear.include? id_unique
+            
+            # count occurrence
+            cards_appear[id_unique] = true
+            $cards_total_count[id] += 1
+            
+            # count win/loss
+            if action["username"] == winner
+                $cards_win_count[id] += 1
+            end
+        }
+    }
+}
+
+data = $cards_total_count.map { |id, total|
+    wincount = $cards_win_count[id]
+    winrate = wincount.to_f / total
+    percent = (winrate * 10000).round / 100.0
+    [id, [
+        id,
+        $database[id],
+        wincount,
+        total,
+        "#{percent}%"
+    ]]
+}.to_h
+
+unless Dir.exist? "data"
+    Dir.mkdir "data"
+end
+
+File.write("data/#{name}.json", data.to_json)
+
+data.to_a.sort_by { |id, data| data[1] }.each { |id, data|
+    puts data.join(";")
+}
