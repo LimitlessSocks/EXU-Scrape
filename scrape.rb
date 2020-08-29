1e9895859c649530e466bc82478fe1bbc59abc23
LimitlessSocks <socktheringleader@gmail.com>
2020-08-29 04:53:03 -0400

    move comb scripts to their own files

--- scrape.rb
+++ scrape.rb
@@ -31,66 +31,9 @@ puts "Loading capybara..."
 $session = Capybara::Session.new(:selenium)
 puts "Loaded!"
 
-$comb_deck_header = <<EOF
-(function(obj) {
-    window.scrapeReady = false;
-    let promises = [];
-    for(let [name, fn] of Object.entries(obj)) {
-        promises.push(new Promise((resolve, reject) => {
-            window[name] = function() {
-                let res = fn();
-                resolve();
-                return res;
-            }
-        }));
-    }
-    Promise.all(promises).then(() => {
-        window.scrapeReady = true;
-    });
-    setTimeout(() => {
-        window.scrapeReady = true;
-    }, 10000);
-})({ countMain: countMain, countSide: countSide, countExtra: countExtra });
-EOF
-    
-$comb_deck_fn = <<EOF
-(function() {
-    if(!window.scrapeReady) {
-        let error = false;
-        try {
-            let message = document.getElementById("msg");
-            error = message.children[1].textContent === "Deck does not exist";
-        }
-        catch(e) {
-            // pass
-        }
-        return {
-            success: false,
-            error: error,
-        };
-    }
-    let results = [];
-    for(let a of [deck_arr, side_arr, extra_arr].flat()) {
-        let { width, height } = a.find("img.pic").css(["width", "height"]);
-        let data = a.data();
-        let src = data.custom > 0 ? CUSTOM_PICS_START : CARD_IMAGES_START;
-        let id = data.id;
-        if(data.custom > 0) {
-            let idMod = id - id % 100000;
-            src += idMod + "/";
-        }
-        src += id + ".jpg" + (data.pic != "1" ? "?version=" + a.pic : "");
-        data.src = src;
-        data.width = width;
-        data.height = height;
-        results.push(data);
-    }
-    return {
-        success: true,
-        results: results,
-    }
-})();
-EOF
+$comb_deck_header = File.read("comb/deck_header.js")
+
+$comb_deck_fn = File.read("comb/deck_fn.js")
 
 def comb_deck(id)
     $session.visit("https://www.duelingbook.com/deck?id=#{id}")
