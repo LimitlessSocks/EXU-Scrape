3b586ab7dddd0c46331b8c69de25fd37965423e8
LimitlessSocks <socktheringleader@gmail.com>
2020-11-07 05:49:30 -0500

    update ycg

--- scrape-book.rb
+++ scrape-book.rb
@@ -11,5 +11,10 @@ data = nil
 while data.nil?
     data = $session.evaluate_script $comb_book
 end
+
+# make a proper database instead of an array
+
+data = data.map { |o| [o["id"], o] }.to_h
+
 File.write("ycg.json", data.to_json)
 STDERR.puts "Done, wrote #{data.size} entries."
