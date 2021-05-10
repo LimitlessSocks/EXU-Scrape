e3aa1100385210b32fd09038dbf3530627ee6d81
LimitlessSocks <socktheringleader@gmail.com>
2021-05-10 18:46:04 -0400

    add bfyf

---
+++ bfyf/list.js
@@ -0,0 +1,181 @@
+// "scrape" editor
+const listOfNamesToArrayFormat = (str) => {
+    let map = new Map();
+    for(let line of str.split(/\r?\n/)) {
+        map.set(line, 0);
+    }
+    for(let [id, card] of Object.entries(CardViewer.Database.cards)) {
+        if(map.get(card.name) === 0) {
+            map.set(card.name, id);
+        }
+    }
+    let s = "[\n";
+    for(let [name, id] of map) {
+        if(id !== 0) {
+            s += `    ${(id + ",").padEnd(10)} // ${name}\n`;
+        }
+    }
+    s += "]";
+    return s;
+};
+
+const BFYF_CARD_IDS = [
+    9023,      // Oops!
+    2487,      // Kuriboh
+    1946,      // Golden Ladybug
+    3461,      // Raigeki Break
+    5116,      // Black Ray Lancer
+    4407,      // Toll
+    5689,      // Bacon Saver
+    5095,      // Number 39: Utopia
+    5069,      // Space Cyclone
+    3336,      // Polymerization
+    207,       // Arcana Force XIV - Temperance
+    4289,      // The Flute of Summoning Kuriboh
+    8886,      // Linkuriboh
+    5115,      // Baby Tiragon
+    1843,      // Giant Germ
+    2772,      // Mask of Darkness
+    4300,      // The Hunter with 7 Weapons
+    5225,      // Chachaka Archer
+    4235,      // Terraforming
+    7120,      // Dragong
+    9024,      // Hallohallo
+    7121,      // Mandragon
+    7855,      // Performapal Odd-Eyes Light Phoenix
+    1998,      // Gravelstorm
+    689,       // Chiron the Mage
+    1233,      // Double Cyclone
+    1311,      // Dust Tornado
+    617,       // Card Trader
+    3682,      // Sakuretsu Armor
+    7219,      // Abyss Stungray
+    6883,      // Quantum Cat
+    6882,      // Swamp Mirrorer
+    5962,      // Shapesister
+    1707,      // Fusion Sage
+    2349,      // Junk Synchron
+    3959,      // Speed Warrior
+    2350,      // Junk Warrior
+    1494,      // Exiled Force
+    3903,      // Snipe Hunter
+    5514,      // Compulsory Escape Device
+    2081,      // Hand Destruction
+    8259,      // Present Card
+    1055,      // Darkness Approaches
+    5796,      // Armored Kappa
+    5114,      // Shining Elf
+    58,        // Alector, Sovereign of Birds
+    4132,      // Susa Soldier
+    6108,      // Aratama
+    2709,      // Maharaghi
+    3233,      // Otohime
+    1108,      // Des Lacooda
+    17,        // A Legendary Ocean
+    5606,      // Abyss Warrior
+    2680,      // Magical Arm Shield
+    1099,      // Depth Amulet
+    9273,      // Cyberse White Hat
+    7,         // 7
+    4509,      // Twin-Barrel Dragon
+    324,       // Barrel Dragon
+    15,        // A Hero Emerges
+    2954,      // Monster Reincarnation
+    7216,      // Jar of Avarice
+    8458,      // Power Wall
+    8065,      // Marshmacaron
+    10060,     // Baba Barber
+    2443,      // Knight of the Red Lotus
+    414,       // Birthright
+    1561,      // Field-Commander Rahz
+    4445,      // Trap Hole
+    3703,      // Sauropod Brachion
+    3953,      // Spawn Alligator
+    4117,      // Super-Ancient Dinobeast
+    4243,      // Teva
+    3849,      // Sinister Serpent
+    4409,      // Tongue Twister
+    2328,      // Jirai Gumo
+    1680,      // Freed the Matchless General
+    8826,      // Fantastic Striborg
+    8868,      // Broken Line
+    3344,      // Pot of Duality
+    2721,      // Majestic Mech - Ohka
+    4692,      // Warm Worm
+    296,       // Aztekipede, the Worm Warrior
+    3103,      // Needle Worm
+    3799,      // Shield Worm
+    6525,      // Resonance Insect
+    270,       // Assault on GHQ
+    664,       // Changer Synchron
+    973,       // Dark Diviner
+    545,       // Brain Crusher
+    481,       // Blast Sphere
+    1579,      // Final Destiny
+    4590,      // Vampire Lord
+    7100,      // Absolute King Back Jack
+    2293,      // Iron Chain Dragon
+    1981,      // Gravedigger Ghoul
+    1090,      // Delg the Dark Monarch
+    4365,      // Thestalos the Firestorm Monarch
+    7821,      // Angmarl the Fiendish Monarch
+    1971,      // Granmarg the Rock Monarch
+    2129,      // Herculean Power
+    3934,      // Soul Release
+    1991,      // Gravekeeper's Servant
+    2751,      // Man-Eater Bug
+    2699,      // Magician of Faith
+    168,       // Ancient Rules
+    9096,      // LANphorhynchus
+    9095,      // Traffic Ghost
+    417,       // Black Brutdrago
+    500,       // Block Attack
+    4067,      // Stop Defense
+    4346,      // The Transmigration Prophecy
+    2679,      // Magical Android
+    1726,      // Gaia Power
+    4552,      // Umiiruka
+    2634,      // Luminous Spark
+    2942,      // Molten Destruction
+    3598,      // Rising Air Current
+    3026,      // Mystic Plasma Zone
+    1219,      // Doom Dozer
+    4739,      // Wetlands
+    9513,      // Agave Dragon
+    3469,      // Rainbow Life
+    1018,      // Dark Ruler Ha Des
+    2813,      // Mefist the Infernal General
+    4268,      // The Dragon Dwelling in the Deep
+    6878,      // Card Advance
+    3794,      // Shield & Sword
+    4382,      // Thunder Dragon
+    4304,      // The Inexperienced Spy
+    4512,      // Twin-Headed Thunder Dragon
+    2155,      // Hiro's Shadow Scout
+    3531,      // Reinforce Truth
+    11,        // A Cat of Ill Omen
+    1143,      // Destruct Potion
+    960,       // Dark Bug
+    1951,      // Good Goblin Housekeeping
+    1962,      // Graceful Dice
+    3866,      // Skull Dice
+    3790,      // Share the Pain
+    7123,      // Tatsunoko
+    6329,      // Blackfeather Darkrage Dragon
+    834,       // Crystal Seer
+    5917,      // Underworld Fighter Balmung
+    3711,      // Scrap Archfiend
+    5916,      // Mist Bird Clausolas
+    733,       // Cockroach Knight
+    6455,      // Cairngorgon, Antiluminescent Knight
+    5683,      // Slushy
+    1500,      // Explosive Magician
+    1754,      // Gather Your Mind
+    6526,      // Breaker the Dark Magical Warrior
+    6039,      // Number 44: Sky Pegasus
+    6816,      // Gagaga Samurai
+    5020,      // Shark Stickers
+    5096,      // Number 17: Leviathan Dragon
+    5109,      // Kachi Kochi Dragon
+    5498,      // Sword Breaker
+];
---
+++ bfyf/main.js
@@ -0,0 +1,257 @@
+window.databaseToUse = "https://raw.githubusercontent.com/LimitlessSocks/EXU-Scrape/master/bfyf.json";
+// window.databaseToUse = "./db.json";
+let onLoad = async function () {
+    let response = await fetch(window.databaseToUse);
+    let db = await response.json();
+    CardViewer.Database.setInitial(db);
+    
+    CardViewer.Elements.searchParameters = $("#searchParameters");
+    
+    CardViewer.Elements.cardType = $("#cardType");
+    CardViewer.Elements.cardLimit = $("#cardLimit");
+    CardViewer.Elements.cardAuthor = $("#cardAuthor");
+    CardViewer.Elements.search = $("#search");
+    CardViewer.Elements.results = $("#results");
+    CardViewer.Elements.autoSearch = $("#autoSearch");
+    CardViewer.Elements.cardName = $("#cardName");
+    CardViewer.Elements.resultCount = $("#resultCount");
+    CardViewer.Elements.cardDescription = $("#cardDescription");
+    CardViewer.Elements.currentPage = $("#currentPage");
+    CardViewer.Elements.pageCount = $("#pageCount");
+    CardViewer.Elements.nextPage = $("#nextPage");
+    CardViewer.Elements.previousPage = $("#previousPage");
+    CardViewer.Elements.resultNote = $("#resultNote");
+    CardViewer.Elements.cardId = $("#cardId");
+    CardViewer.Elements.cardCategory = $("#cardCategory");
+    CardViewer.Elements.cardVisibility = $("#cardVisibility");
+    CardViewer.Elements.ifMonster = $(".ifMonster");
+    CardViewer.Elements.ifSpell = $(".ifSpell");
+    CardViewer.Elements.ifTrap = $(".ifTrap");
+    CardViewer.Elements.ifLink = $(".ifLink");
+    CardViewer.Elements.cardSpellKind = $("#cardSpellKind");
+    CardViewer.Elements.cardTrapKind = $("#cardTrapKind");
+    CardViewer.Elements.monsterStats = $("#monsterStats");
+    CardViewer.Elements.spellStats = $("#spellStats");
+    CardViewer.Elements.trapStats = $("#trapStats");
+    CardViewer.Elements.cardLevel = $("#cardLevel");
+    CardViewer.Elements.cardMonsterCategory = $("#cardMonsterCategory");
+    CardViewer.Elements.cardMonsterAbility = $("#cardMonsterAbility");
+    CardViewer.Elements.cardMonsterType = $("#cardMonsterType");
+    CardViewer.Elements.cardMonsterAttribute = $("#cardMonsterAttribute");
+    CardViewer.Elements.cardATK = $("#cardATK");
+    CardViewer.Elements.cardDEF = $("#cardDEF");
+    CardViewer.Elements.cardLevelCompare = $("#cardLevelCompare");
+    CardViewer.Elements.cardATKCompare = $("#cardATKCompare");
+    CardViewer.Elements.cardDEFCompare = $("#cardDEFCompare");
+    CardViewer.Elements.toTopButton = $("#totop");
+    CardViewer.Elements.saveSearch = $("#saveSearch");
+    CardViewer.Elements.clearSearch = $("#clearSearch");
+    
+    CardViewer.Elements.search.click(CardViewer.submit);
+    CardViewer.Elements.previousPage.click(CardViewer.Search.previousPage);
+    CardViewer.Elements.nextPage.click(CardViewer.Search.nextPage);
+    
+    CardViewer.Elements.toTopButton.click(() => {
+        $("html, body").animate(
+            { scrollTop: "0px" },
+            { duration: 200, }
+        );
+    });
+    
+    CardViewer.Elements.saveSearch.click(() => {
+        let strs = [];
+        for(let [key, value] of Object.entries(CardViewer.query())) {
+            if(value !== "" && value !== "any" && key !== "imported" && key !== "notImported" && key !== "alsoImported") {
+                if(key === "retrain" && !value) continue;
+                if(key.indexOf("Compare") !== -1 && value === "equal") continue;
+                strs.push(key + "=" + value);
+            }
+        }
+        if(strs.length || window.location.search) {
+            window.location.search = encodeURI(strs.join(","))
+                .replaceAll(".", "%2E")
+                .replaceAll(",", "%2C");
+        }
+    });
+    
+    const KeyToElement = {
+        name:               CardViewer.Elements.cardName,
+        effect:             CardViewer.Elements.cardDescription,
+        type:               CardViewer.Elements.cardType,
+        limit:              CardViewer.Elements.cardLimit,
+        id:                 CardViewer.Elements.cardId,
+        author:             CardViewer.Elements.cardAuthor,
+        // retrain:         CardViewer.Elements.cardIsRetrain,
+        category:           CardViewer.Elements.cardCategory,
+        visibility:         CardViewer.Elements.cardVisibility,
+        monsterType:        CardViewer.Elements.cardMonsterType,
+        monsterAttribute:   CardViewer.Elements.cardMonsterAttribute,
+        monsterCategory:    CardViewer.Elements.cardMonsterCategory,
+        monsterAbility:     CardViewer.Elements.cardMonsterAbility,
+        level:              CardViewer.Elements.cardLevel,
+        levelCompare:       CardViewer.Elements.cardLevelCompare,
+        atk:                CardViewer.Elements.cardATK,
+        atkCompare:         CardViewer.Elements.cardATKCompare,
+        def:                CardViewer.Elements.cardDEF,
+        defCompare:         CardViewer.Elements.cardDEFCompare,
+    };
+    
+    const parseStringValue = (str) => {
+        if(str === "true" || str === "false") {
+            return str === "true";
+        }
+        
+        let tryInt = parseInt(str);
+        if(!Number.isNaN(tryInt)) {
+            return tryInt;
+        }
+        
+        return str;
+    }
+    
+    if(window.location.search) {
+        let searchString = decodeURI(
+            window.location.search.replaceAll("%2C", ",")
+        )
+        CardViewer.firstTime = false;
+        let type = null;
+        for(let pair of searchString.slice(1).split(",")) {
+            pair = pair.replace(
+                /%([0-9A-Fa-f]{2})/g,
+                (_, $1) => String.fromCharCode(parseInt($1, 16))
+            );
+            let [ key, value ] = pair.split(/=(.+)?/);
+            let el = KeyToElement[key];
+            if(!el) {
+                if(key === "kind") {
+                    if(type === "spell") {
+                        el = CardViewer.Elements.cardSpellKind
+                    }
+                    else {
+                        el = CardViewer.Elements.cardTrapKind;
+                    }
+                }
+                else if(key === "arrowMask") {
+                    let buttons = $(".arrow-button");
+                    let mask = parseInt(value, 2);
+                    
+                    for(let i = 0; i < flatArrow.length; i++) {
+                        let subMask = flatArrow[i];
+                        // console.log(subMask, i, flatArrow[i], mask);
+                        if(subMask && (mask & subMask) === subMask) {
+                            $(buttons[i]).toggleClass("toggled");
+                        }
+                    }
+                    continue;
+                }
+                else if(key === "exactArrows") {
+                    if(value === "true") {
+                        $("#equals").toggleClass("toggled");
+                    }
+                    continue;
+                }
+                else if(key === "group" || key === "alsoImported") {
+                    continue;
+                }
+            }
+            value = parseStringValue(value);
+            if(el.is("[type='checkbox']")) {
+                el.prop("checked", value);
+            }
+            else {
+                el.val(value);
+            }
+            if(key === "type") {
+                type = value;
+            }
+        }
+    }
+    
+    CardViewer.Elements.autoSearch.change(function () {
+        CardViewer.autoSearch = this.checked;
+    });
+    CardViewer.Elements.autoSearch.change();
+    
+    CardViewer.setUpTabSearchSwitching();
+    CardViewer.setUpArrowToggle();
+    
+    let allInputs = CardViewer.setUpAllInputs();
+    
+    // check if any input data
+    for(let el of allInputs) {
+        let hasValue = false;
+        el = $(el);
+        if(el.is("select")) {
+            if(el.val() != el.children().first().val()) {
+                hasValue = true;
+            }
+        }
+        else if(el.is("input[type=checkbox]")) {
+            console.log(el.attr("checked"));
+            hasValue = el.is(":checked");
+        }
+        else {
+            hasValue = el.val() != "";
+        }
+        if(hasValue) {
+            CardViewer.firstTime = false;
+            break;
+        }
+    }
+    
+    CardViewer.submit();
+    
+    let updateBackground = () => {
+        if(localStorage.getItem("EXU_REDOX_MODE") === "true") {
+            $("html").css("background-image", "url(\"" + getResource("bg", "godzilla") + "\")");
+            $("html").css("background-size", "100% 100%");
+        }
+        else {
+            $("html").css("background-image", "");
+            $("html").css("background-size", "");
+        }
+    };
+    
+    $(window).keydown((ev) => {
+        let orig = ev.originalEvent;
+        if(ev.altKey && ev.key === "R") {
+            let wasActive = localStorage.getItem("EXU_REDOX_MODE") === "true";
+            localStorage.setItem("EXU_REDOX_MODE", !wasActive);
+            updateBackground();
+        }
+    });
+    
+    updateBackground();
+    
+    const purposeFilter = $("#purposeFilter");
+    
+    for(let obj of Object.values(CardGroups)) {
+        let { id, name, data } = obj;
+        let button = $("<button>");
+        button.text(name);
+        button.click(() => {
+            CardViewer.firstTime = false;
+            CardViewer.Elements.resultNote.text("This is an incomplete list. You can help by finding new cards in this category that are semi-generic, that is, able to be used by more than 1 particular strategy.");
+            CardViewer.demonstrate((card) => data.indexOf(card.id) !== -1);
+        });
+        button.contextmenu((e) => {
+            window.location.search = "group=" + id;
+            e.preventDefault();
+        });
+        button.toggle();
+        purposeFilter.append(button);
+        obj.button = button;
+    }
+    
+    let match = window.location.search.match(/group=(\w+)/);
+    if(match) {
+        // console.log(match[1]);
+        CardGroups[match[1]].button.click();
+    }
+    
+    $("#expandPurpose, #contractPurpose").click(function () {
+        $("#purposeFilter button").toggle();
+    });
+};
+
+window.addEventListener("load", onLoad);
