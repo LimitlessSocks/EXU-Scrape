c39be2c6fec3a2840e070aed4a9469f4e0a07303
LimitlessSocks <socktheringleader@gmail.com>
2021-08-16 23:02:10 -0400

    add listing

---
+++ gen-dir.rb
@@ -0,0 +1,22 @@
+$BASE = "log-out"
+$INDEX_PRE_PATH = File.join $BASE, "index-pre.html"
+$INDEX_OUT_PATH = File.join $BASE, "index.html"
+
+content = File.read($INDEX_PRE_PATH)
+listing = Dir[File.join $BASE, "*.html"]
+listing.reject! { |path| /index/ =~ path }
+
+html = ""
+html += "<body>\n"
+html += "  <h1>List of update logs</h1>\n"
+html += "  <ul>"
+listing.each { |path|
+    link = File.basename path, ".html"
+    html += "    <li><code><a href=\"./#{link}\">#{path}</a></code></li>"
+}
+html += "  </ul>"
+html += "</body>"
+
+content.sub!(/<body><\/body>/, html)
+
+File.write $INDEX_OUT_PATH, content
