a5ea53c227054d81b1c8e8408f5c98594f6441de
LimitlessSocks <socktheringleader@gmail.com>
2022-04-19 23:51:58 -0400

    add credit

--- credit.js
+++ credit.js
@@ -29,4 +29,36 @@ window.addEventListener("load", async function () {
     sofar.textContent = trueCount;
     customtotal.textContent = total;
     percent.style.color = `rgb(${255 - 100 * ratio}, ${100 + 150 * ratio}, 100)`;
+    
+    // modified https://stackoverflow.com/a/49041392/4119004
+    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+    const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+        v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+        )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+    // do the work...
+    document.querySelectorAll('th').forEach(th => th.addEventListener('click', function(ev) {
+        console.log(this);
+        const table = th.closest('table');
+        
+        // remove and reset other tags
+        table.querySelectorAll("th").forEach(th => {
+            th.classList.remove("descending");
+            th.classList.remove("ascending");
+            if(th !== this) {
+                th.asc = false;
+            }
+        });
+        
+        Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+            .forEach(tr => table.appendChild(tr) );
+        if(this.asc) {
+            this.classList.add("ascending");
+        }
+        else {
+            this.classList.add("descending");
+        }
+    }));
 });
