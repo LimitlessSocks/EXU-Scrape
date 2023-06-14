0a7f992193503ad17ba2315285f7987646818ec7
LimitlessSocks <socktheringleader@gmail.com>
2023-06-13 21:44:01 -0400

    abstract HTML credit listing to a json file

--- credit.js
+++ credit.js
@@ -8,6 +8,60 @@ window.addEventListener("load", async function () {
     const per = document.getElementById("percent");
     const sofar = document.getElementById("sofar");
     const customtotal = document.getElementById("customtotal");
+    
+    // add credits from JSON
+    let unread = await fetch("./credit.json");
+    let creditContent = await unread.json();
+    let creditsElement = document.querySelector("#credits tbody");
+    for(let [ id, data ] of Object.entries(creditContent)) {
+        console.log(data);
+        let tr = document.createElement("tr");
+        let cardName = document.createElement("td");
+        let cardId = document.createElement("td");
+        let artSource = document.createElement("td");
+        let artist = document.createElement("td");
+        let notes = document.createElement("td");
+        
+        cardName.textContent = data.name;
+        
+        let cardIdAnchor = document.createElement("a");
+        cardIdAnchor.textContent = data.id;
+        cardIdAnchor.href = `./card?id=${data.id}`;
+        cardId.appendChild(cardIdAnchor);
+        
+        let { href, text } = data.src;
+        if(href || text) {
+            let artSourceAnchor = document.createElement("a");
+            artSourceAnchor.textContent = text;
+            artSourceAnchor.href = href;
+        }
+        
+        artist.textContent = data.artist;
+        if(data.links.length) {
+            artist.textContent += " (";
+            data.links.forEach(({ href, text}, index) => {
+                let artistLinkAnchor = document.createElement("a");
+                artistLinkAnchor.textContent = text;
+                artistLinkAnchor.href = href;
+                artist.appendChild(artistLinkAnchor);
+                let after = index + 1 === data.links.length
+                    ? ")"
+                    : " \u22c5 ";
+                artist.appendChild(document.createTextNode(after));
+            });
+        }
+        
+        notes.textContent = data.notes;
+        
+        tr.appendChild(cardName);
+        tr.appendChild(cardId);
+        tr.appendChild(artSource);
+        tr.appendChild(artist);
+        tr.appendChild(notes);
+        
+        creditsElement.appendChild(tr);
+    }
+    
     let ids = [...document.querySelectorAll("#credits tr td:nth-child(2)")]
         .map(e=>e.textContent);
     
